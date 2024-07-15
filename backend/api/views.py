3901f8e998d25a05b65105abd9c09c5ae5335aa9
Hiro Funatsuka <hiro.funatsuka@gmail.com>
2024-07-15 17:50:19 +0900

    Implement CRUD for tasks

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,38 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, TaskSerializer
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from .models import Task
 
 # Create your views here.
+
+
+class TaskListCreate(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Task.objects.filter(author=user)
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+
+class TaskDelete(generics.DestroyAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Task.objects.filter(author=user)
+
+    # def perform_destroy(self, instance):
+    #     instance.delete()
 
 
 class CreateUserView(generics.CreateAPIView):
